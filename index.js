8b77b923580e9cecd6b0e7f61c29e1ad7410ba77
michelleykim <yeojin011016@gmail.com>
2024-01-09 23:45:38 -0800

    initial FE setup

---
+++ index.js
@@ -0,0 +1,11 @@
+let showModels = () => {
+	console.log("should show models");
+};
+
+let generateImage = () => {
+	console.log("should generate and show image");
+};
+
+let generateResponse = () => {
+	console.log("should generate and show response");
+};
