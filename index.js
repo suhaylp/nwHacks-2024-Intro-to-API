fa5f479b0b299d3f6ec54d0364ed422dc107cffe
ichung08 <isaacchung1990@gmail.com>
2024-01-11 16:58:18 -0800

    added backend

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+import { getModels, createChat } from "./api.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('showModelsButton').addEventListener('click', () => {
+        console.log("should show models");
+        getModels();
+    });
+
+	document.getElementById('generateImageButton').addEventListener('click', () => {
+        console.log("should generate and show image");
+        // createImage();
+    });
+
+    document.getElementById('generateResponseButton').addEventListener('click', () => {
+        const inputText = document.getElementById('text-prompt').value;
+        console.log("Input text:", inputText);
+        createChat(inputText);
+    });
+
+    // Add similar event listeners for other buttons
+});
+
 let showModels = () => {
 	console.log("should show models");
+	getModels();
 };
 
 let generateImage = () => {
