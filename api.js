7f8f66628885e679dfe15d9a8a2174be24608cc2
ichung08 <isaacchung1990@gmail.com>
2024-01-16 08:31:14 -0800

    added fill in the blanks

--- api.js
+++ api.js
@@ -8,16 +8,13 @@ export async function getModels() {
     const secrets = await loadSecrets();
     const apiKey = await secrets.apiKey;
 
-    const response = await fetch("https://api.openai.com/v1/models", {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${apiKey}`,
-            "Content-Type": "application/json"
-        }
-    });
-    const models = await response.json();
-    // Update the webpage with the models
-    updateModelList(models.data);
+    // Fill this in yourself!
+    // TODO: Fill in the fetch call
+    
+
+
+    // TODO: Fill in the rest of the function
+
 }
 
 function updateModelList(models) {
@@ -38,32 +35,25 @@ export async function createChat(inputText) {
     const secrets = await loadSecrets();
     const apiKey = await secrets.apiKey;
 
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${apiKey}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "model": "gpt-3.5-turbo",
-            "messages": [
-              {
-                "role": "system",
-                "content": "You are a helpful assistant."
-              },
-              {
-                "role": "user",
-                "content": inputText
-              }
-            ]
-        })
-    });
-    const chat = await response.json();
-    // Update the answer box with the response
-    updateAnswerBox(chat.choices[0].message.content);
+    // Fill this in yourself!
+    // TODO: Fill in the fetch call
+
+
+
+    // TODO: Fill in the rest of the function
+
 }
 
 function updateAnswerBox(responseText) {
     const answerBox = document.getElementById('generated-answer');
     answerBox.textContent = responseText;
+}
+
+// Endpoint 3 
+
+export async function create_FILL_THIS_IN() {
+    const secrets = await loadSecrets();
+    const apiKey = await secrets.apiKey;
+
+    // Fill this in yourself!
 }
